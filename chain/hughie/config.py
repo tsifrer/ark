8535709be189d3e731acafd0f87bd2cbf81a90b3
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2019-03-21 16:35:48 +0100

    huey redis settings

--- chain/hughie/config.py
+++ chain/hughie/config.py
@@ -1,4 +1,11 @@
+import os
+
 from huey import RedisHuey
 
 # TODO: config yo
-huey = RedisHuey('hughie', host='localhost')
+huey = RedisHuey(
+    'hughie',
+    host=os.environ.get('REDIS_HOST', 'localhost'),
+    port=os.environ.get('REDIS_PORT', 6379),
+    db=os.environ.get('REDIS_DB', 0),
+)
